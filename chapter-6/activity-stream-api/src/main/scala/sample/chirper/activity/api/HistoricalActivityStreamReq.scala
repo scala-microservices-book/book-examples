7699382697e8908baa06282ec82aa3e031776669
Jatin Puri <purijatin@gmail.com>
2017-07-24 02:55:28 +0530

    rename folder

--- chapter-6/activity-stream-api/src/main/scala/sample/chirper/activity/api/HistoricalActivityStreamReq.scala
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
- */
-package sample.chirper.activity.api
-
-import java.time.Instant
-
-case class HistoricalActivityStreamReq(fromTime: Instant)
